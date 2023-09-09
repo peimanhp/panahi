cc8c4821933c1d8c5f0eb250f11865fdd6ac79f8
Peiman Hosseini <107307767+peimanhp@users.noreply.github.com>
2023-09-09 14:06:33 +0330

    nav heght updated

--- view/header.php
+++ view/header.php
@@ -1,81 +1,50 @@
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>وب سایت شخصی عارفه پناهی</title>
-    <link rel="stylesheet" href="http://localhost/panahi/assets/css/style.css" />
-    <link
-      href="http://localhost/panahi/assets/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
-      crossorigin="anonymous"
-    />
-  </head>
-  <body>
-    <header>
-      <nav dir="rtl" class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <a class="navbar-brand" href="#"
-            ><img class="logo" src="http://localhost/panahi/assets/img/logo.png" alt="logo"
-          /></a>
 
-          <div
-            class="offcanvas offcanvas-end"
-            tabindex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">لوگو</h5>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
-                <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="#">صفحه اصلی</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">دوره ها</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">بلاگ</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">درباره من</a>
-                </li>
-              </ul>
-              <form
-                class="d-flex position-relative align-items-center"
-                role="search"
-              >
-                <input
-                  class="form-control me-2 search-bar"
-                  type="search"
-                  placeholder="جست و جو ..."
-                  aria-label="Search"
-                />
-                <button class="btn btn-outline-none search-btn" type="submit">
-                  <img src="http://localhost/panahi/assets/img/search-normal.svg" alt="" />
-                </button>
-              </form>
-            </div>
-          <!-- </div> -->
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>وب سایت شخصی عارفه پناهی</title>
+  <link rel="stylesheet" href="http://localhost/panahi/assets/css/style.css" />
+  <link href="http://localhost/panahi/assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+</head>
+
+<body>
+  <header>
+    <nav dir="rtl" class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#"><img class="logo" src="http://localhost/panahi/assets/img/logo.png" alt="logo" /></a>
+
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">لوگو</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">صفحه اصلی</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">دوره ها</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">بلاگ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">درباره من</a>
+              </li>
+            </ul>
+            <form class="d-flex position-relative align-items-center" role="search">
+              <input class="form-control me-2 search-bar" type="search" placeholder="جست و جو ..." aria-label="Search" />
+              <button class="btn btn-outline-none search-btn" type="submit">
+                <img src="http://localhost/panahi/assets/img/search-normal.svg" alt="" />
+              </button>
+            </form>
+          </div>
         </div>
         <div>
           <button class="btn">
@@ -83,12 +52,8 @@
           </button>
           <button class="btn login-btn">ورود / ثبت نام</button>
         </div>
-      </nav>
-</header>
-    <script src="http://localhost/panahi/assets/js/main.js"></script>
-    <script
-      src="http://localhost/panahi/assets/js/bootstrap.bundle.min.js"
-      integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
-      crossorigin="anonymous"
-    ></script>
-
+      </div>
+    </nav>
+  </header>
+  <script src="http://localhost/panahi/assets/js/main.js"></script>
+  <script src="http://localhost/panahi/assets/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
