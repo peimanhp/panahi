562301a5651ebb40564cbd41689ea93c2266ac89
Peiman Hosseini <107307767+peimanhp@users.noreply.github.com>
2023-09-10 10:58:49 +0330

    @media desktop added

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <section class="first-section text-center">
     <div class="row row-gap-4">
         <div class="col-12 col-md-6">
-            <h2 class="mt-5">آکادمی آموزش تبلیغات</h2>
+            <h2 class="mt-5">آکادمی آموزش تبلیغات</h2>            
             <h1>عارفه <span class="red-title">پناهــــی</span></h1>
             <h3 class="sub-title">بهترین و بروز ترین دوره‌های آموزش‌
                 جامع تبلیغات</h3>
@@ -14,7 +14,7 @@
         </div>
         <div class="col-12 col-md-6">
             <div class="mx-auto">
-                <img class="panahi-photo rounded-4" src="./assets/img/panahi.jpg" alt="panahi pic">
+                <img class="panahi-photo " src="./assets/img/panahi.jpg" alt="panahi pic">
             </div>
         </div>
     </div>
