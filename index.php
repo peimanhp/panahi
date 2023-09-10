73f4efb71d26d887e8357f42675203d585df1786
Peiman Hosseini <107307767+peimanhp@users.noreply.github.com>
2023-09-10 15:48:55 +0330

    first section done

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php include_once 'view/header.php' ?>
 
-<section class="first-section text-center">
-    <div class="row row-gap-4">
-        <div class="col-12 col-md-6">
+<section class="first-section">
+    <div class="row row-gap-4 container">
+        <div class="col-12 col-md-6 d-flex justify-content-start">
+        <div class="text-center titles">
             <h2 class="mt-5">آکادمی آموزش تبلیغات</h2>            
             <h1>عارفه <span class="red-title">پناهــــی</span></h1>
             <h3 class="sub-title">بهترین و بروز ترین دوره‌های آموزش‌
                 جامع تبلیغات</h3>
             <div class="d-flex justify-content-center gap-3 pt-3">
-                <button class="btn first-section-btns btn-gray px-auto py-3">مشاهده دوره ها</button>
-                <button class="btn first-section-btns btn-red px-auto py-3">دربــــاره من</button>
+                <button class="btn first-section-btns btn-gray py-3">مشاهده دوره ها</button>
+                <button class="btn first-section-btns btn-red py-3">دربـــــــــــــاره من</button>
             </div>
         </div>
-        <div class="col-12 col-md-6">
-            <div class="mx-auto">
-                <img class="panahi-photo " src="./assets/img/panahi.jpg" alt="panahi pic">
+        </div>
+        <div class="col-12 col-md-6 p-0">
+            <div class="photo-wrapper">
+                <img class="panahi-photo" src="./assets/img/panahi.jpg" alt="panahi pic">
             </div>
         </div>
     </div>
--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,5 @@
 <script src="http://localhost/panahi/assets/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script src="http://localhost/panahi/assets/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
