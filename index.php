2ff3fdb85fcbf49f777cb18a2ee5495f6eea055a
Peiman Hosseini <107307767+peimanhp@users.noreply.github.com>
2023-09-11 12:04:00 +0330

    section B build

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 <section class="first-section">
     <div class="row row-gap-4 container">
         <div class="col-12 col-md-6 titles-wrapper">
-        <div class="text-center titles">
-            <h2 class="mt-5">آکادمی آموزش تبلیغات</h2>            
-            <h1>عارفه <span class="red-title">پناهــــی</span></h1>
-            <h3 class="sub-title">بهترین و بروز ترین دوره‌های آموزش‌
-                جامع تبلیغات</h3>
-            <div class="d-flex justify-content-center gap-3 pt-3">
-                <button class="btn first-section-btns btn-gray py-3">مشاهده دوره ها</button>
-                <button class="btn first-section-btns btn-red py-3">دربـــــــــــــاره من</button>
+            <div class="text-center titles">
+                <h2 class="mt-5">آکادمی آموزش تبلیغات</h2>
+                <h1>عارفه <span class="red-title">پناهــــی</span></h1>
+                <h3 class="sub-title">بهترین و بروز ترین دوره‌های آموزش‌
+                    جامع تبلیغات</h3>
+                <div class="d-flex justify-content-center gap-3 pt-3">
+                    <button class="btn first-section-btns btn-gray py-3">مشاهده دوره ها</button>
+                    <button class="btn first-section-btns btn-red py-3">دربـــــــــــــاره من</button>
+                </div>
             </div>
-        </div>
         </div>
         <div class="col-12 col-md-6 p-0">
             <div class="photo-wrapper">
@@ -20,6 +20,56 @@
             </div>
         </div>
     </div>
+</section>
+<section class="second-section container mb-5">
+    <div class="row row-gap-4">
+        <div class="col col-12 col-md-3 px-3">
+            <h3 class="courses-title mb-4 ">دوره های آموزشی</h3>
+            <p class="courses-content">
+                لورم ایپسوم متن ساختگی با تولید
+                سادگی نامفهوم ازصنعت چاپ و با استفاده
+                از طراحان گرافیک است.
+            </p>
+        </div>
+        <div class="col col-12 col-md-3 d-flex flex-column align-items-center">
+            <div class="course-card-content">
+                <img src="./assets/img/package-img.png" alt="package-img">
+                <div class="d-flex align-items-center gap-1">
+                    <img src="./assets/img/profile-circle.svg" alt="author">
+                    <p class="mb-0">مدرس: عارفه پناهی</p>
+                </div>
+                <h3 class="course-name">دوره آموزشی شماره سه</h3>
+                <h4 class="price">4.000.000 تومان</h4>
+                <button class="btn btn-red">مشاهده</button>
+            </div>
+        </div>
+        <div class="col col-12 col-md-3 d-flex flex-column align-items-center">
+            <div class="course-card-content">
+                <img src="./assets/img/package-img.png" alt="package-img">
+                <div class="d-flex align-items-center gap-1">
+                    <img src="./assets/img/profile-circle.svg" alt="author">
+                    <p class="mb-0">مدرس: عارفه پناهی</p>
+                </div>
+                <h3 class="course-name">دوره آموزشی شماره سه</h3>
+                <h4 class="price">4.000.000 تومان</h4>
+                <button class="btn btn-red">مشاهده</button>
+            </div>
+        </div>
+        <div class="col col-12 col-md-3 d-flex flex-column align-items-center">
+            <div class="course-card-content">
+                <img src="./assets/img/package-img.png" alt="package-img">
+                <div class="d-flex align-items-center gap-1">
+                    <img src="./assets/img/profile-circle.svg" alt="author">
+                    <p class="mb-0">مدرس: عارفه پناهی</p>
+                </div>
+                <h3 class="course-name">دوره آموزشی شماره سه</h3>
+                <h4 class="price">4.000.000 تومان</h4>
+                <button class="btn btn-red">مشاهده</button>
+            </div>
+        </div>
+        
+    </div>
+
 </section>
 
 <?php include_once 'view/footer.php' ?>
