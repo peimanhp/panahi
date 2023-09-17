bb766035bd8e08c29ebddc1a6c1409808f1e2619
Peiman Hosseini <107307767+peimanhp@users.noreply.github.com>
2023-09-17 14:57:26 +0330

    login done

--- view/header.php
+++ view/header.php
@@ -5,11 +5,6 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>وب سایت شخصی عارفه پناهی</title>
-<<<<<<< HEAD
-  <link rel="stylesheet" href="../../../panahi/assets/css/style.css" />
-  <link href="../../../panahi/assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
-  <link href="../../../panahi/assets/css/splide.min.css" rel="stylesheet">
-=======
 
   <link rel="stylesheet" href="./assets/css/style.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
@@ -17,7 +12,6 @@
   <link rel="stylesheet" href="http://localhost/panahi/assets/css/style.css" />
   <link href="http://localhost/panahi/assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
   <link href="http://localhost/panahi/assets/css/splide.min.css" rel="stylesheet">
->>>>>>> f7f0af2c152ed32d53b8fed3167730d05c590f9a
 </head>
 
 <body>
