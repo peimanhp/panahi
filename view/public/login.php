180824a913a1832188f358f26486e0fd6fcbff84
Peiman Hosseini <107307767+peimanhp@users.noreply.github.com>
2023-09-18 10:31:33 +0330

    login desktop view added

--- view/public/login.php
+++ view/public/login.php
@@ -1,21 +1,34 @@
 <?php include_once '../header.php' ?>
 
-<section class="position-relative">
-    <img class="red-circle-login" src="../../assets/img/red-circle.svg" alt="">
-    <img class="girl-photo" src="../../assets/img/girl-login.svg" alt="">
-</section>
-<section class="container">
-    <form class="form-sms mx-auto" action="#">
-        <h1 class="text-center login-title">تایید رمز</h1>
-        <div class="row mb-3">
-            <div class="col-6 d-flex justify-content-end"><button class="btn btn-red btn-resend">ارسال مجدد</button></div>
-            <div class="col-6"><input class="input input-time" type="text" value="120 ثانیه" disabled></div>
-        </div>
-        <input class="input input-code mb-3" type="text" placeholder="کد دریافتی 4 رقمی">
-        <button class="btn btn-red btn-sms mb-3">ادامه</button>
-        <p>شماره را اشتباه وارد کردید؟ <a class="red-link" href="#">ویرایش شماره</a></p>
-    </form>
-</section>
+<div class="desktop-view mt-5">
+    <div class="login-wrapper">
+        <section class="container">
+            <form class="form-sms mx-auto" action="#">
+                <h1 class="text-center login-title">ورود</h1>
+                <p class="text-login">جهت ورود یا ثبت نام در سایت، شماره موبایل خود را با دقت وارد کنید تا یک پیام حاوی کد فعال سازی برایتان ارسال شود. (فقط کاربران جمهوری اسلامی ایران)</p>
+                <input class="input input-code mb-3" type="text" placeholder="شماره موبایل">
+                <button class="btn btn-red btn-sms mb-3">ارسال رمز یکبار مصرف</button>
+            </form>
+        </section>
+        <section class="girl-wrapper">            
+            <img class="" src="../../assets/img/boy-login.svg" alt="">
+        </section>
+    </div>
+</div>
+<div class="mobile-view">
+    <section class="position-relative">
+        <img class="red-circle-login" src="../../assets/img/red-circle.svg" alt="">
+        <img class="girl-photo" src="../../assets/img/boy-login.svg" alt="">
+    </section>
+    <section class="container">
+        <form class="form-sms mx-auto" action="#">
+            <h1 class="text-center login-title">ورود</h1>
+            <p class="text-login">جهت ورود یا ثبت نام در سایت، شماره موبایل خود را با دقت وارد کنید تا یک پیام حاوی کد فعال سازی برایتان ارسال شود. (فقط کاربران جمهوری اسلامی ایران)</p>
+            <input class="input input-code mb-3" type="text" placeholder="شماره موبایل">
+            <button class="btn btn-red btn-sms mb-3">ارسال رمز یکبار مصرف</button>
+        </form>
+    </section>
+</div>
 
 
 
