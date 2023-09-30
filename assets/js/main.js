d15bfbd5c87cf45af16c8e112bf2d5267d06a085
Peiman Hosseini <107307767+peimanhp@users.noreply.github.com>
2023-09-30 16:10:32 +0330

    courses page completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,5 @@ splide.mount();
 
 
 
+
+
--- view/public/courses.php
+++ view/public/courses.php
@@ -63,23 +63,32 @@
       <h1 class="my-5 text-light page-title">دوره های آموزشــــــی</h1>
     </div>
   </section>
-  <section class="second-section container mb-5">
-  <div class="px-3">
-        <h3 class="courses-title mt-5 mb-4 ">دوره های آموزشی</h3>
-        <p class="courses-content">
-          لورم ایپسوم متن ساختگی با تولید
-          سادگی نامفهوم ازصنعت چاپ و با استفاده
-          از طراحان گرافیک است.
-        </p>        
-      </div>
-    <div class="row row-gap-4">      
-      <div class="col col-12 col-md-6 px-5">
+  <section class="second-section container">
+    <div class="border-dashed pb-3 px-3 mb-5">
+      <h3 class="courses-title mt-5 mb-4 ">دوره های <span class="text-red">تبلیغات</span></h3>
+      <p class="courses-content">
+        لورم ایپسوم متن ساختگی با تولید
+        سادگی نامفهوم ازصنعت چاپ و با استفاده
+        از طراحان گرافیک است.
+      </p>
+    </div>
+    <div class="row row-gap-4">
+    <div class="col col-12 col-md-6 px-5">
         <div class="d-flex justify-content-center">
           <div class="course-card-content">
             <img class="slider-img" src="/panahi/assets/img/product-sample.png" alt="package-img">
-            <div class="d-flex align-items-center gap-1">
-              <img src="/panahi/assets/img/profile-circle.svg" alt="author">
-              <p class="mb-0">مدرس: عارفه پناهی</p>
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <div class="d-flex align-items-center gap-1">
+                <img src="/panahi/assets/img/profile-circle.svg" alt="author">
+                <p class="mb-0">مدرس: عارفه پناهی</p>
+              </div>
+              <div dir="ltr" class="d-flex">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/unstar.svg" alt="stars">
+              </div>
             </div>
             <h3 class="course-name">دوره آموزشی شماره یک</h3>
             <h4 class="price">4.000.000 تومان</h4>
@@ -91,11 +100,78 @@
         <div class="d-flex justify-content-center">
           <div class="course-card-content">
             <img class="slider-img" src="/panahi/assets/img/product-sample.png" alt="package-img">
-            <div class="d-flex align-items-center gap-1">
-              <img src="/panahi/assets/img/profile-circle.svg" alt="author">
-              <p class="mb-0">مدرس: عارفه پناهی</p>
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <div class="d-flex align-items-center gap-1">
+                <img src="/panahi/assets/img/profile-circle.svg" alt="author">
+                <p class="mb-0">مدرس: عارفه پناهی</p>
+              </div>
+              <div dir="ltr" class="d-flex">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/unstar.svg" alt="stars">
+              </div>
+            </div>
+            <h3 class="course-name">دوره آموزشی شماره دو</h3>
+            <h4 class="price">4.000.000 تومان</h4>
+            <button class="btn btn-red">مشاهده</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="third-section container mb-5">
+    <div class="border-dashed pb-3 px-3 mb-5">
+      <h3 class="courses-title mt-5 mb-4 ">دوره های <span class="text-red">فروش</span></h3>
+      <p class="courses-content">
+        لورم ایپسوم متن ساختگی با تولید
+        سادگی نامفهوم ازصنعت چاپ و با استفاده
+        از طراحان گرافیک است.
+      </p>
+    </div>
+    <div class="row row-gap-4">
+    <div class="col col-12 col-md-6 px-5">
+        <div class="d-flex justify-content-center">
+          <div class="course-card-content">
+            <img class="slider-img" src="/panahi/assets/img/product-sample.png" alt="package-img">
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <div class="d-flex align-items-center gap-1">
+                <img src="/panahi/assets/img/profile-circle.svg" alt="author">
+                <p class="mb-0">مدرس: عارفه پناهی</p>
+              </div>
+              <div dir="ltr" class="d-flex">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/unstar.svg" alt="stars">
+              </div>
             </div>
             <h3 class="course-name">دوره آموزشی شماره یک</h3>
+            <h4 class="price">4.000.000 تومان</h4>
+            <button class="btn btn-red">مشاهده</button>
+          </div>
+        </div>
+      </div>
+      <div class="col col-12 col-md-6 px-5">
+        <div class="d-flex justify-content-center">
+          <div class="course-card-content">
+            <img class="slider-img" src="/panahi/assets/img/product-sample.png" alt="package-img">
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <div class="d-flex align-items-center gap-1">
+                <img src="/panahi/assets/img/profile-circle.svg" alt="author">
+                <p class="mb-0">مدرس: عارفه پناهی</p>
+              </div>
+              <div dir="ltr" class="d-flex">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/full-star.svg" alt="stars">
+                <img class="star ms-1" src="/panahi/assets/img/unstar.svg" alt="stars">
+              </div>
+            </div>
+            <h3 class="course-name">دوره آموزشی شماره دو</h3>
             <h4 class="price">4.000.000 تومان</h4>
             <button class="btn btn-red">مشاهده</button>
           </div>
@@ -105,9 +181,8 @@
   </section>
 
 
-  <script type="module" src="/panahi/assets/js/main.js"></script>
+  <script type="module" src="/panahi/assets/js/courses.js"></script>
   <script src="/panahi/assets/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-  <script src="/panahi/assets/js/splide.min.js"></script>
 </body>
 
 </html>
